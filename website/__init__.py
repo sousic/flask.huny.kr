084cd2669de5e5d992ceae962999e49275df2aa6
sousic <sousics@naver.com>
2016-08-05 22:14:11 +0900

    #flask-login 테스트 코드 추가

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,43 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
+from flask_login import LoginManager, UserMixin
 
 app = Flask(__name__)
 app.config.from_object('websiteconfig')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    pass
+
+users = { 'test' : {'pw' : 'test'}}
+
+
+@login_manager.user_loader
+def user_loader(id):
+    if id not in users:
+        return
+
+    user = User()
+    user.id = id
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['id'] == users[id]
+
+    return user
 
 from views.home import home
 app.register_blueprint(home.mod)
--- website/views/home.py
+++ website/views/home.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
+import flask_login
 from flask import Flask, Blueprint, render_template, redirect, url_for, request
+from website import User
+
 
 class home:
     mod = Blueprint('home', __name__)
@@ -8,7 +11,19 @@ class home:
     def home():
         return render_template('home/index.html', title = 'Home')
 
-    @mod.route('/login/', methods = ['POST','GET'])
+    @mod.route('/login/')
     def login():
-        if request.METHOD == 'GET':
-            return redirect(url_for('/'))
+        #if request.method == 'GET':
+        user = User()
+        user.id = 'test'
+        user.state = 'admin'
+
+        flask_login.login_user(user)
+        return ''
+        #return redirect(url_for('/'))
+
+    @mod.route("/check/")
+    @flask_login.login_required
+    def login_check():
+        print flask_login._get_user().id, flask_login._get_user().state
+        return ''
