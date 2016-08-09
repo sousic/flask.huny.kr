4d3bb08660812cb80f65162908d72b49d846549e
sousic <sousics@naver.com>
2016-08-09 14:12:32 +0900

    회원가입 부분 작업중

--- website/views/member.py
+++ website/views/member.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 
 from website import common_dao
 from website.domain.UserVO import UserVO
@@ -9,7 +9,7 @@ from website.persistence.memberDAO import memberDAO
 class member():
     mod = Blueprint('member', __name__, url_prefix='/member')
 
-    @mod.route('/regist/', methods=['GET','POST'])
+    @mod.route('/regist', methods=['GET','POST'])
     def regist():
         if request.method == 'POST':
             user = UserVO()
@@ -24,3 +24,25 @@ class member():
             return redirect(url_for('home.main'))
 
         return render_template('member/regist.html', title='Register', nav_title='Register')
+
+    @mod.route('/idcheck', methods=['POST'])
+    def idcheck():
+        result = dict(result=0,msg='')
+        if request.method == 'POST':
+            user = UserVO()
+            user.id = request.form['userid']
+
+            member_dao = memberDAO(common_dao)
+            user_id = member_dao.memberIdCheck(user)
+
+            if user_id is None:
+                result['result'] = -1
+                return jsonify(result)
+            else:
+                result['result'] = 1
+                return jsonify(dict(result=1))
+
+        result['result'] = -1
+        result['msg'] = 'Not UserID'
+
+        return jsonify(result)
